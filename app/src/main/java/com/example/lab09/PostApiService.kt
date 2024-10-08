105c2e2a9c9447b881d5652cd91f7970aa4871c5
deyito1 <space123580@gmail.com>
2024-10-08 13:13:21 -0500

    primer comit hasta el 4

---
+++ app/src/main/java/com/example/lab09/PostApiService.kt
@@ -0,0 +1,10 @@
+package com.example.lab09
+
+interface PostApiService {
+    @GET("posts")
+    suspend fun getUserPosts(): List<PostModel>
+
+    @GET("posts/{id}")
+    suspend fun getUserPostById(@Path("id") id: Int): PostModel
+
+}
---
+++ app/src/main/java/com/example/lab09/PostModel.kt
@@ -0,0 +1,8 @@
+package com.example.lab09
+
+data class PostModel(
+    @SerializedName("userId") val userId: Int,
+    @SerializedName("id") val id: Int,
+    @SerializedName("title") val title: String,
+    @SerializedName("body") val body: String
+)
